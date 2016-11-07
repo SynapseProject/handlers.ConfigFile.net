1e246499e86a06e1e46f2a559dfafc62c5afff95
Guy Waguespack <guy.waguespack@bp.com>
2016-11-07 09:36:49 -0600

    Initial Check-in of Dummy Handler

---
+++ Synapse.Handler.CommandLine/NewSynapseHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+using Synapse.Core;
+
+namespace Synapse.Handler.ConfigFile
+{
+    public class NewSynapseHandler : HandlerRuntimeBase
+    {
+        override public ExecuteResult Execute(HandlerStartInfo startInfo)
+        {
+            return new ExecuteResult() { Status = StatusType.Complete };
+        }
+    }
+}
